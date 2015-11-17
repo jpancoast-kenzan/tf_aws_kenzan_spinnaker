ceef23f54619f73d27a8b1cccfac452c98ce46e3
James Pancoast <jpancoast@kenzanmedia.com>
2015-11-17 13:56:17 -0700

    removed some commented out sections in main.tf

--- main.tf
+++ main.tf
@@ -8,43 +8,9 @@ variable "storagetype" {
   default = "instance-store"
 }
 
-
-#{
-#  "variable": {
-#    "all_amis": {
-#      "description": "The AMI to use",
-#      "default": {
-#        "ap-northeast-1-karmic-amd64-pv-instance-store": "ami-2a0fa42b",
-#        "ap-northeast-1-karmic-i386-pv-instance-store": "ami-240fa425",
-#        "ap-southeast-1-karmic-amd64-pv-instance-store": "ami-90344ac2",
-#
-
 output "ami_id" {
     value = "${lookup(var.all_amis, format(\"%s-%s-%s-%s-%s\", var.region, var.distribution, var.architecture, var.virttype, var.storagetype))}"
 }
-
-
-#{
-#	"variable": {
-#		"azs": {
-#			"description": "AZs per region",
-#			"default": {
-#				"us-west-2": "a:b:c"
-#			}
-#		}
-#	}
-#}
-
-#{
-#	"variable": {
-#		"az_counts": {
-#			"description": "AZ counts per region",
-#			"default": {
-#				"us-west-2": "3"
-#			}
-#		}
-#	}
-#}
 
 output "azs_per_region" {
 	value = "${lookup(var.azs, var.region)}"
